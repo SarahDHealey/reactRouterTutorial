3143058085884311d1a4521d9b5b09d0575b2bb1
Sarah Healey <s.deane.healey@gmail.com>
2017-04-23 14:01:14 -0600

    working on lesson02

--- lessons/02-rendering-a-route/index.js
+++ lessons/02-rendering-a-route/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
+import { Router, Route, hashHistory } from 'react-router'
 render(<App/>, document.getElementById('app'))
