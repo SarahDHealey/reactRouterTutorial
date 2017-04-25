764beb1b137fbbeac4a8ab90341600e15c2f4f22
Sarah Healey <s.deane.healey@gmail.com>
2017-04-24 22:20:34 -0600

    adding in simplified example to server.js

--- lessons/13-server-rendering/server.js
+++ lessons/13-server-rendering/server.js
@@ -37,6 +37,26 @@ app.get('*', (req, res) => {
   })
 })
 
+// This is the simplified version
+
+// send all requests to index.html so browserHistory works
+
+// app.get('*', (req, res) => {
+  // match the routes to the url
+  // match({ routes: routes, location: req.url }, (err, redirect, props) => {
+    // `RouterContext` is what the `Router` renders. `Router` keeps these
+    // `props` in its state as it listens to `browserHistory`. But on the
+    // server our app is stateless, so we need to use `match` to
+    // get these props before rendering.
+    // const appHtml = renderToString(<RouterContext {...props}/>)
+
+    // dump the HTML into a template, lots of ways to do this, but none are
+    // really influenced by React Router, so we're just using a little
+    // function, `renderPage`
+    // res.send(renderPage(appHtml))
+  // })
+// })
+
 function renderPage(appHtml) {
   return `
     <!doctype html public="storage">
